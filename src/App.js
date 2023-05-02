af3c91b0b82bcc55ccb43d1a37c54feda06ccafd
AMBERNAY TROTTER <AMBERNAYTROTTER@GMAIL.COM>
2023-05-01 21:20:18 -0400

    testing for api and added boilerplate css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 
 function App() {
+
+    const apiKey = '0a093f521e98a991f4e4cc2a12460255';
+
+    const url = new URL('https://api.themoviedb.org/3/movie/550?api_key=0a093f521e98a991f4e4cc2a12460255');
+
+    // url.search = new URLSearchParams({
+    //     title: "Legally Blonde",
+    //     key: apiKey
+    // })
+
+    fetch(url)
+        .then(results => {
+            return results.json();
+        })
+        .then(data => {
+            console.log(data);
+            // movieData(data.artObjects)
+        })
+
   return (
     <>
         <h1>Movie Picker App</h1>
