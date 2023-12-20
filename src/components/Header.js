87418bd13c35bd742a6d4ca75183a7c4359f8134
AMBERNAY TROTTER <AMBERNAYTROTTER@GMAIL.COM>
2023-12-20 12:23:21 -0500

    fixed hover on 'find movie' button

--- src/components/Header.js
+++ src/components/Header.js
@@ -34,21 +34,23 @@ function Header({ isDropdownVisible, setIsDropdownVisible, isTrending, setIsTren
                 <div className="wrapper">
                     <div className="heading-container">
                         <h1>Movie Picker</h1>
-                        <button className="find-movie-button" onClick={handleDropdown} >
-                            <figure>
-                                <span className="hover-animation">
-                                    <h2>Find
-                                        <span> {tvMovieToggle === 'movie' ? ' Movies' : ' Shows'} </span>
-                                    </h2>
-                                </span>
-                                <svg xmlns="http://www.w3.org/2000/svg" className={"ionicon " + arrowClass} viewBox="0 0 512 512"><path d="M98 190.06l139.78 163.12a24 24 0 0036.44 0L414 190.06c13.34-15.57 2.28-39.62-18.22-39.62h-279.6c-20.5 0-31.56 24.05-18.18 39.62z" /></svg>
-                                <figcaption className="sr-only">{arrowClass}</figcaption>
-                            </figure>
-                        </button>
-                        <label className="switch">
-                            <input type="checkbox" onClick={handleTvMovieToggle} />
-                            <span className="slider round"></span>
-                        </label>
+                        <div className="header-buttons-container">
+                            <button className="find-movie-button" onClick={handleDropdown} >
+                                <figure>
+                                    <span className="hover-animation">
+                                        <h2>Find
+                                            <span> {tvMovieToggle === 'movie' ? ' Movies' : ' Shows'} </span>
+                                        </h2>
+                                    </span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className={"ionicon " + arrowClass} viewBox="0 0 512 512"><path d="M98 190.06l139.78 163.12a24 24 0 0036.44 0L414 190.06c13.34-15.57 2.28-39.62-18.22-39.62h-279.6c-20.5 0-31.56 24.05-18.18 39.62z" /></svg>
+                                    <figcaption className="sr-only">{arrowClass}</figcaption>
+                                </figure>
+                            </button>
+                            <label className="switch">
+                                <input type="checkbox" onClick={handleTvMovieToggle} />
+                                <span className="slider round"></span>
+                            </label>
+                        </div>
                     </div>
                 </div>{/* wrapper */}
             </section>
