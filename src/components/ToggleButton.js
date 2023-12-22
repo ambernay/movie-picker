d2a34784f88bd80d4d14e45c3c45a97963c09980
AMBERNAY TROTTER <AMBERNAYTROTTER@GMAIL.COM>
2023-12-22 14:04:57 -0500

    chnaged rocker button back to toggle with complete styles, made toggle icons accessible

--- src/components/ToggleButton.js
+++ src/components/ToggleButton.js
@@ -9,14 +9,18 @@ function ToggleButton({ tvMovieToggle, setTvMovieToggle }) {
     }
 
     return (
-        <div className="toggle-button-container" onClick={handleTvMovieToggle}>
-            <div id={tvMovieToggle === 'movie' ? "activated-option" : "deactivated-option"} className="toggle-left">
-                <svg className="ionicon" viewBox="0 0 512 512"><path d="M464 384.39a32 32 0 01-13-2.77 15.77 15.77 0 01-2.71-1.54l-82.71-58.22A32 32 0 01352 295.7v-79.4a32 32 0 0113.58-26.16l82.71-58.22a15.77 15.77 0 012.71-1.54 32 32 0 0145 29.24v192.76a32 32 0 01-32 32zM268 400H84a68.07 68.07 0 01-68-68V180a68.07 68.07 0 0168-68h184.48A67.6 67.6 0 01336 179.52V332a68.07 68.07 0 01-68 68z" /></svg>
-            </div>
-            <div id={tvMovieToggle === 'tv' ? "activated-option" : "deactivated-option"} className="toggle-right">
+        <figure id={tvMovieToggle === 'movie' ? "movie-option" : "tv-option"} className="toggle-button-container" onClick={handleTvMovieToggle}>
+            <span className="slider"></span>
+            <figure id={tvMovieToggle === 'movie' ? "activated-option" : "deactivated-option"} className="toggle-left movie-icon">
+                <svg className="ionicon" viewBox="0 0 512 512"><path d="M436 80H76a44.05 44.05 0 00-44 44v264a44.05 44.05 0 0044 44h360a44.05 44.05 0 0044-44V124a44.05 44.05 0 00-44-44zM112 388a12 12 0 01-12 12H76a12 12 0 01-12-12v-24a12 12 0 0112-12h24a12 12 0 0112 12zm0-80a12 12 0 01-12 12H76a12 12 0 01-12-12v-24a12 12 0 0112-12h24a12 12 0 0112 12zm0-80a12 12 0 01-12 12H76a12 12 0 01-12-12v-24a12 12 0 0112-12h24a12 12 0 0112 12zm0-80a12 12 0 01-12 12H76a12 12 0 01-12-12v-24a12 12 0 0112-12h24a12 12 0 0112 12zm241.68 124H158.32a16 16 0 010-32h195.36a16 16 0 110 32zM448 388a12 12 0 01-12 12h-24a12 12 0 01-12-12v-24a12 12 0 0112-12h24a12 12 0 0112 12zm0-80a12 12 0 01-12 12h-24a12 12 0 01-12-12v-24a12 12 0 0112-12h24a12 12 0 0112 12zm0-80a12 12 0 01-12 12h-24a12 12 0 01-12-12v-24a12 12 0 0112-12h24a12 12 0 0112 12zm0-80a12 12 0 01-12 12h-24a12 12 0 01-12-12v-24a12 12 0 0112-12h24a12 12 0 0112 12z" /></svg>
+                <figcaption className="sr-only">film reel, for movie option</figcaption>
+            </figure>
+            <figure id={tvMovieToggle === 'tv' ? "activated-option" : "deactivated-option"} className="toggle-right tv-icon">
                 <svg className="ionicon" viewBox="0 0 512 512"><path d="M447.86 384H64.14A48.2 48.2 0 0116 335.86V128.14A48.2 48.2 0 0164.14 80h383.72A48.2 48.2 0 01496 128.14v207.72A48.2 48.2 0 01447.86 384z" /><path strokeLinecap="round" strokeMiterlimit="10" strokeWidth="32" d="M128 416h256" /></svg>
-            </div>
-        </div>
+                <figcaption className="sr-only">TV screen, for TV option</figcaption>
+            </figure>
+            <figcaption className="sr-only">Movie vs TV toggle switch</figcaption>
+        </figure>
     )
 };
 
