e7a14e8b5fc87c509a7225a3aedfcfadbaa9520e
AMBERNAY TROTTER <AMBERNAYTROTTER@GMAIL.COM>
2024-03-06 12:18:28 -0500

    removed 'where to watch' fieldset container

--- src/components/MainSection/ProviderIconsList.js
+++ src/components/MainSection/ProviderIconsList.js
@@ -62,41 +62,37 @@ function ProviderIconsList({ movieTitle, movieID, viewingOptions }) {
     return (
         <div className='wheretowatch-container'>
             <h4 className='wheretowatch-heading'>{movieTitle}</h4>
-            <fieldset className='main-provider-fieldset'>
-                <legend className='main-provider-label'>Where to watch</legend>
+            <ul className='viewing-options-list-container'>
+                {Object.keys(viewingOptions).sort().map((key) => {
+                    const imageURL = 'https://image.tmdb.org/t/p/w500';
+                    // create lists
+                    const optionKey = key + '/' + movieID;
+                    return (
+                        <li className='option' key={optionKey}>
+                            <fieldset className='provider-list-fieldsets'>
+                                <legend>{filteredKey(key)}</legend>
+                                <ul className='provider-options-list-container'>
+                                    {/* create icons */}
+                                    {viewingOptions[key]?.map((key, i) => {
+                                        const iconKey = i + '/' + movieID + '/' + key.provider_id + key.logo_path;
 
-                <ul className='viewing-options-list-container'>
-                    {Object.keys(viewingOptions).sort().map((key) => {
-                        const imageURL = 'https://image.tmdb.org/t/p/w500';
-                        // create lists
-                        const optionKey = key + '/' + movieID;
-                        return (
-                            <li className='option' key={optionKey}>
-                                <fieldset className='provider-list-fieldsets'>
-                                    <legend>{filteredKey(key)}</legend>
-                                    <ul className='provider-options-list-container'>
-                                        {/* create icons */}
-                                        {viewingOptions[key]?.map((key, i) => {
-                                            const iconKey = i + '/' + movieID + '/' + key.provider_id + key.logo_path;
-
-                                            return (
-                                                <li key={iconKey}>
-                                                    {(key.logo_path === 'N/A') ?
-                                                        <h4>{key.logo_path}</h4>
-                                                        :
-                                                        <img className='provider-icons' src={imageURL + key.logo_path} alt={key.provider_name} />
-                                                    }
-                                                </li>
-                                            )
-                                        })
-                                        }
-                                    </ul>
-                                </fieldset>
-                            </li>
-                        )
-                    })}
-                </ul>
-            </fieldset>
+                                        return (
+                                            <li key={iconKey}>
+                                                {(key.logo_path === 'N/A') ?
+                                                    <h4>{key.logo_path}</h4>
+                                                    :
+                                                    <img className='provider-icons' src={imageURL + key.logo_path} alt={key.provider_name} />
+                                                }
+                                            </li>
+                                        )
+                                    })
+                                    }
+                                </ul>
+                            </fieldset>
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     )
 }
