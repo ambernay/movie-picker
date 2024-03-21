7d5015d9aa031a759da27814758f3a41d6c63e38
AMBERNAY TROTTER <AMBERNAYTROTTER@GMAIL.COM>
2024-03-21 10:59:55 -0400

    added sr-only label and placeholder to search input, tried to get auto focus for input

--- src/components/HeaderSection/SearchBar.js
+++ src/components/HeaderSection/SearchBar.js
@@ -1,14 +1,21 @@
 // 'https://api.themoviedb.org/3/search/movie?query=Jack+Reacher&api_key=0a093f521e98a991f4e4cc2a12460255'
 
-import { useState, memo } from 'react';
+import { useState, useRef, useEffect, memo } from 'react';
 import { MagnifyerIcon } from '../Icons';
 
 function SearchBar() {
-
     const [newValue, setNewValue] = useState('');
     const [isOpen, setIsOpen] = useState('');
 
     const inputClass = isOpen ? 'input-container' : 'hidden';
+
+    const searchInput = useRef(null);
+
+    // useEffect(() => {
+    //     if (isOpen && searchInput.current) {
+    //         searchInput.current.focus();
+    //     }
+    // }, [isOpen]);
 
     const handleInput = (e) => {
         setNewValue(e.target.value);
@@ -16,14 +23,25 @@ function SearchBar() {
     }
     const handleIconClick = (e) => {
         setIsOpen(!isOpen);
+        searchInput.current.focus();
     }
 
     return (
-        <div className='search-container' onClick={handleIconClick}>
+        <div className='search-container' >
             <div className='wrapper'>
-                <MagnifyerIcon />
+                <div className={'search-icon-container'} onClick={handleIconClick}>
+                    <MagnifyerIcon />
+                </div>
                 <div className={inputClass}>
-                    <input value={newValue} onChange={handleInput}></input>
+                    <label name={'movie search'} className={'sr-only'}>Search movies by keyword</label>
+                    <input
+                        placeholder={'Search by keyword...'}
+                        name={'movie search'}
+                        value={newValue}
+                        onChange={handleInput}
+                        // onBlur setIsOpen causes infinite loop
+                        ref={searchInput}>
+                    </input>
                     <button className='search-button' >
                         <MagnifyerIcon />
                     </button>
